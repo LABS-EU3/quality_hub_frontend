940af807a0ca4f36d685e3af047042dafaf6f796
Dom <domeccleston@gmail.com>
2019-12-19 15:02:59 +0000

    landing page styling: middle section nearly done

--- quality_hub/src/components/Landing.jsx
+++ quality_hub/src/components/Landing.jsx
@@ -10,30 +10,43 @@ import React from 'react';
 
 import logo from '../img/firelogo.png';
 const vector2 = require('../img/landingvector.png');
+const greenBackgroundSVG = require('../img/green-slanted-bg-shape.png');
 
 // styled components
-const Logo = styled.div`
-  height: 10em;
-  width: 5em;
-  background-image: url(${logo});
-  background-repeat: no-repeat;
+
+const LandingWrapper = styled.div`
+  width: 100%;
 `;
+
 const NavbarContainer = styled.div`
-  height: 5em;
-  width: 100vw;
+  height: 4em;
+  width: 100%;
   background: white;
   display: flex;
   justify-content: space-between;
+  position: sticky;
+  top: 0;
+  z-index: 11;
+  box-shadow: 0px 3px 4px -2px rgba(150, 150, 150, 1);
+`;
+
+const Logo = styled.div`
+  width: 4.5em;
+  height: 5em;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  transform: scale(0.7);
 `;
 
 const LogoTitleContainer = styled.div`
   width: 30%;
   display: flex;
   justify-content: flex-start;
+  align-items: center;
 `;
 
 const NavTitleContainer = styled.div`
-  font-family: Ubuntu;
+  font-family: 'Ubuntu';
   font-style: normal;
   font-weight: normal;
   color: #459a59;
@@ -45,11 +58,12 @@ const LinksContainer = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  padding-right: 0.2em;
 `;
 
 const NavbarLink = styled.div`
   font-family: Ubuntu;
-  font-size: 18px;
+  font-size: 14px;
   color: black;
 
   a {
@@ -60,25 +74,30 @@ const NavbarLink = styled.div`
 
 export const buttonTheme = {
   text: 'white',
-  background: '#4fad65'
+  background: '#408f53'
 };
 
 export const invertTheme = {
   text: '#4fad65',
   background: 'white'
-}
+};
 
 export const StyledButton = styled.button`
   background: ${props => props.theme.background};
   color: ${props => props.theme.text};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 4px;
-  height: 4em;
-  width: 8em;
+  height: 3em;
+  width: 7em;
   padding: 1em;
   border: none;
   font-family: Ubuntu;
   font-size: 14px;
+  transition: opacity 0.2s;
+
+  :hover {
+    opacity: 0.9;
+  }
 
   :focus {
     outline: none;
@@ -86,13 +105,15 @@ export const StyledButton = styled.button`
 
   :active {
     outline: none;
-    transform: translateY(2px);
+    transform: translateY(1px);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
   }
 `;
 
 const TopLandingContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  padding-top: 1em;
+  height: 60vh;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
@@ -106,15 +127,15 @@ const LandingTaglineContainer = styled.div`
   align-items: center;
 
   h1 {
-    width: 100%;
+    width: 80%;
     font-size: 40px;
-    margin-left: 4em;
-    padding-top: 1em;
+    padding-top: 0.75em;
   }
 
   h3 {
+    margin-top: 0em;
     width: 80%;
-    font-size: 32px;
+    font-size: 24px;
     font-weight: normal;
   }
 `;
@@ -132,10 +153,17 @@ const SignupContainer = styled.div`
     border: 1px solid #c8c8c8;
     padding: 0.5em;
     font-size: 16px;
-    color: #808080;
+
+    :hover {
+      background: #edf2f7;
+    }
 
     :focus {
       outline: none;
+    }
+
+    ::placeholder {
+      color: darkgray;
     }
   }
 
@@ -157,9 +185,58 @@ const LandingVectorImageContainer = styled.div`
   z-index: 10;
 
   img {
-    height: 40em;
-    width: 40em;
+    padding-right: 3em;
+    height: 29em;
+    width: 29em;
   }
+`;
+
+const InfoboxContainer = styled.div`
+  height: 120vh;
+  background-image: url(${greenBackgroundSVG});
+  background-repeat: no-repeat;
+  background-size: 100vw 120vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+`;
+
+const InfoBoxTitleContainer = styled.div`
+  width: 100%;  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: white;
+    padding-bottom: 1em;
+  }
+`
+
+const InfoCardsContainer = styled.div`
+  display: flex;
+  margin-top: -10em;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-evenly;
+  align-items: flex-start;
+`
+
+const BottomLandingContainer = styled.div`
+  height: 100vh;
+`;
+
+const InfoCard = styled.div`
+  height: 20em;
+  width: 16em;
+  background: white;
+  border: 1px solid #a3a3a3;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px; 
+  display: flex;
+  flex-direction: column;
 `;
 
 // react components
@@ -167,56 +244,74 @@ const LandingVectorImageContainer = styled.div`
 const SignUp = () => {
   return (
     <SignupContainer>
-      <input />
+      <input placeholder="Enter your email" />
       <StyledButton theme={buttonTheme}>Get Started</StyledButton>
     </SignupContainer>
   );
 };
 
+const LandingCard = () => {
+  return <InfoCard></InfoCard>;
+};
+
 const Landing = () => {
   return (
     <div>
-      <NavbarContainer>
-        <LogoTitleContainer>
-          <Logo />
-          <NavTitleContainer>
-            <h1>DevCoach</h1>
-          </NavTitleContainer>
-        </LogoTitleContainer>
-        <LinksContainer>
-          <NavbarLink>
-            <a href="#">FAQ</a>
-          </NavbarLink>
-          <NavbarLink>
-            <a href="#">About</a>
-          </NavbarLink>
-          <a href="/login">
-            <StyledButton theme={buttonTheme}>LOGIN</StyledButton>
-          </a>
-          <a href="/register">
-            <StyledButton theme={invertTheme}>SIGN UP</StyledButton>
-          </a>
-        </LinksContainer>
-      </NavbarContainer>
-      <TopLandingContainer>
-        <LandingTaglineContainer>
-          <h1>
-            INTERVIEWING ISN'T EASY. THAT DOESN'T MEANT IT HAS TO BE STRESSFUL.
-          </h1>
-          <h3>
-            We connect developers looking to improve their interviewing
-            technique with experienced pros who have mastered the technical
-            interview and can coach you through the process from start to
-            finish.
-          </h3>
-          <SignUp />
-        </LandingTaglineContainer>
-        <LandingRightContainer>
-          <LandingVectorImageContainer>
-            <img src={vector2} />
-          </LandingVectorImageContainer>
-        </LandingRightContainer>
-      </TopLandingContainer>
+      <LandingWrapper>
+        <NavbarContainer>
+          <LogoTitleContainer>
+            <Logo />
+            <NavTitleContainer>
+              <h1>DevCoach</h1>
+            </NavTitleContainer>
+          </LogoTitleContainer>
+          <LinksContainer>
+            <NavbarLink>
+              <a href="#">FAQ</a>
+            </NavbarLink>
+            <NavbarLink>
+              <a href="#">About</a>
+            </NavbarLink>
+            <a href="/login">
+              <StyledButton theme={buttonTheme}>LOGIN</StyledButton>
+            </a>
+            <a href="/register">
+              <StyledButton theme={invertTheme}>SIGN UP</StyledButton>
+            </a>
+          </LinksContainer>
+        </NavbarContainer>
+        <TopLandingContainer>
+          <LandingTaglineContainer>
+            <h1>
+              INTERVIEWING ISN'T EASY. THAT DOESN'T MEANT IT HAS TO BE
+              STRESSFUL.
+            </h1>
+            <h3>
+              We connect developers looking to improve their interviewing
+              technique with experienced pros who have mastered the technical
+              interview and can coach you through the process from start to
+              finish.
+            </h3>
+            <SignUp />
+          </LandingTaglineContainer>
+          <LandingRightContainer>
+            <LandingVectorImageContainer>
+              <img src={vector2} />
+            </LandingVectorImageContainer>
+          </LandingRightContainer>
+        </TopLandingContainer>
+        <InfoboxContainer>
+          <InfoBoxTitleContainer>
+            <h1>Say goodbye to second guessing.</h1>
+          </InfoBoxTitleContainer>
+          <InfoCardsContainer>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+            <InfoCard></InfoCard>
+          </InfoCardsContainer>
+        </InfoboxContainer>
+        <BottomLandingContainer></BottomLandingContainer>
+      </LandingWrapper>
     </div>
   );
 };
