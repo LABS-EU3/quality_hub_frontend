04de21cb31d2811bc79d966791fd994effb37f21
Dom <domeccleston@gmail.com>
2019-12-20 08:53:17 +0000

    landing

--- quality_hub/src/components/Landing.jsx
+++ quality_hub/src/components/Landing.jsx
@@ -1,14 +1,7 @@
 import styled, { css } from 'styled-components';
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-
 import logo from '../img/firelogo.png';
+
 const vector2 = require('../img/landingvector.png');
 const greenBackgroundSVG = require('../img/green-slanted-bg-shape.png');
 const handshakeImg = require('../img/handshake.png');
